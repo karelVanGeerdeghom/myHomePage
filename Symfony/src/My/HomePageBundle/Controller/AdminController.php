fe36b7f42075a20f2b6ed3c5659dece292be16aa
karelVanGeerdeghom <karel.van.geerdeghom@gmail.com>
2015-01-05 15:32:52 +0100

    homepage portfolio upload

--- Symfony/src/My/HomePageBundle/Controller/AdminController.php
+++ Symfony/src/My/HomePageBundle/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace My\HomePageBundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use My\HomePageBundle\Entity\Document;
+use My\HomePageBundle\Entity\Site;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller {
@@ -13,10 +14,13 @@ class AdminController extends Controller {
     }
     
     public function uploadAction(Request $request) {
-        $document = new Document();
+        $site = new Site();
         
-        $form = $this->createFormBuilder($document)
+        $form = $this->createFormBuilder($site)
             ->add('name')
+            ->add('description')
+            ->add('link')
+            ->add('active')
             ->add('file')
             ->add('Save', 'submit')
             ->getForm();
@@ -26,9 +30,9 @@ class AdminController extends Controller {
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
 
-            $document->upload();
+            $site->upload();
 
-            $em->persist($document);
+            $em->persist($site);
             $em->flush();
 
             return $this->redirect($this->generateUrl('admin'));
@@ -39,25 +43,84 @@ class AdminController extends Controller {
     
     public function upload()
     {
-        // the file property can be empty if the field is not required
+
         if (null === $this->getFile()) {
             return;
         }
-
-        // use the original file name here but you should
-        // sanitize it at least to avoid any security issues
-
-        // move takes the target directory and then the
-        // target filename to move to
         $this->getFile()->move(
             $this->getUploadRootDir(),
             $this->getFile()->getClientOriginalName()
         );
-
-        // set the path property to the filename where you've saved the file
         $this->path = $this->getFile()->getClientOriginalName();
-
-        // clean up the file property as you won't need it anymore
         $this->file = null;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    public function uploadAction(Request $request) {
+//        $document = new Document();
+//        
+//        $form = $this->createFormBuilder($document)
+//            ->add('name')
+//            ->add('file')
+//            ->add('Save', 'submit')
+//            ->getForm();
+//        
+//        $form->handleRequest($request);
+//        
+//        if ($form->isValid()) {
+//            $em = $this->getDoctrine()->getManager();
+//
+//            $document->upload();
+//
+//            $em->persist($document);
+//            $em->flush();
+//
+//            return $this->redirect($this->generateUrl('admin'));
+//        }
+//        
+//        return $this->render('MyHomePageBundle:Admin:upload.html.twig', array('form' => $form->createView(),));
+//    }
+//    
+//    public function upload()
+//    {
+//        // the file property can be empty if the field is not required
+//        if (null === $this->getFile()) {
+//            return;
+//        }
+//
+//        // use the original file name here but you should
+//        // sanitize it at least to avoid any security issues
+//
+//        // move takes the target directory and then the
+//        // target filename to move to
+//        $this->getFile()->move(
+//            $this->getUploadRootDir(),
+//            $this->getFile()->getClientOriginalName()
+//        );
+//
+//        // set the path property to the filename where you've saved the file
+//        $this->path = $this->getFile()->getClientOriginalName();
+//
+//        // clean up the file property as you won't need it anymore
+//        $this->file = null;
+//    }
 }
--- Symfony/src/My/HomePageBundle/Controller/PortfolioController.php
+++ Symfony/src/My/HomePageBundle/Controller/PortfolioController.php
@@ -3,11 +3,17 @@
 namespace My\HomePageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use My\HomePageBundle\Entity\Site;
 
 class PortfolioController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('MyHomePageBundle:Portfolio:index.html.twig');
+    public function indexAction() {
+        
+        $sites = $this->getDoctrine()
+                ->getRepository('MyHomePageBundle:Site')
+                ->findAll();
+        
+        
+        return $this->render('MyHomePageBundle:Portfolio:index.html.twig', array('sites' => $sites));
     }
 }
